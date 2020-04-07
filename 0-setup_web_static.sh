83a566399967e2f7db0af9ab94a936e46a4a6dde
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-04-07 17:03:47 -0500

    [Add] Use the sed under sever_name sentence

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,6 +8,6 @@ mkdir -p /data/web_static/releases/test/
 echo "<html>\n\t<head>\n\t</head>\n\t<body>\n\tHolberton School\n\t</body>\n</html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sed -i "56i\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n" /etc/nginx/sites-available/default
+sed -i "s/server_name _;/server_name _;\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n/" /etc/nginx/sites-available/default
 service ngnix start
 exit 0
