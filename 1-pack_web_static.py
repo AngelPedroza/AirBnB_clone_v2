074f34a9cff56001ae0b748f6e99e8aa234747e3
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-04-08 01:04:50 -0500

    [Add] Use fabric.api. Use hide for hide commands while running. < and capture is for save the output

---
+++ 1-pack_web_static.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Create a folder if not exists and create a tgz
+file with the local command execution with fabric"""
+
+from datetime import datetime
+from fabric.api import local, hide
+
+def do_pack():
+    """Create the folder and the tgz"""
+    try:
+        with hide('running'):
+            local("mkdir versions")
+
+        date = datetime.now()
+        all_date = date.strftime("%Y%m%d%H%M%S")
+
+        command = "tar -cvzf versions/web_static_{}.tgz web_static".format(all_date)
+        path = "versions/web_static_{}.tgz".format(all_date)
+        message = "Packing web_static to {}".format(path)
+
+        print(message)
+        local(command)
+
+        with hide('running'):
+            size = local('wc -c < {}'.format(path), capture=True)
+
+        f_msg = "web_static packed: {} -> {}Bytes".format(path, size)
+
+        with hide('running'):
+            local("chmod 664 {}".format(path))
+
+        print(f_msg)
+
+        return path
+    except:
+        return None
