8127a5b832501ff8fd0613f7023af52929d85722
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-04-10 12:08:28 -0500

    [Fix] Remove the exit and the first service

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,7 +2,6 @@
 # Write a Bash script that sets up your web servers for the deployment of web_static
 sudo apt-get update -y
 sudo apt-get install nginx -y
-sudo service nginx start
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
 echo "\
@@ -17,4 +16,3 @@ ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sed -i "38i location /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n/" /etc/nginx/sites-available/default
 sudo service nginx start
-exit 0
