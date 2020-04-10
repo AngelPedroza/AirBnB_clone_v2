06b2a202f36499eaeeb2429cc3f2dca864366198
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-04-10 11:44:24 -0500

    [Fix] Service and string

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,9 +5,17 @@ sudo apt-get install nginx -y
 service nginx start
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
-echo "<html>\n\t<head>\n\t</head>\n\t<body>\n\tHolberton School\n\t</body>\n</html>" > /data/web_static/releases/test/index.html
+echo "\
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
+>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sed -i "s/server_name _;/server_name _;\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n/" /etc/nginx/sites-available/default
-service ngnix start
+service nginx start
 exit 0
