e56dcad0846201adc918051c55a22450dd562fa0
AngelPedroza <angelomarpedrozacardenas@gmail.com>
2020-04-10 12:36:19 -0500

    [Fix] pep8

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,6 +8,7 @@ import os
 
 env.hosts = ['35.237.103.2', '18.209.63.81']
 
+
 def do_pack():
     """Create the folder and the tgz"""
     try:
@@ -15,10 +16,10 @@ def do_pack():
             local("mkdir -p ./versions")
 
         date = datetime.now()
-        all_date = date.strftime("%Y%m%d%H%M%S")
+        adt = date.strftime("%Y%m%d%H%M%S")
 
-        command = "tar -cvzf versions/web_static_{}.tgz web_static".format(all_date)
-        path = "versions/web_static_{}.tgz".format(all_date)
+        command = "tar -cvzf versions/web_static_{}.tgz web_static".format(adt)
+        path = "versions/web_static_{}.tgz".format(adt)
         message = "Packing web_static to {}".format(path)
 
         print(message)
@@ -38,6 +39,7 @@ def do_pack():
     except:
         return None
 
+
 def do_deploy(archive_path):
     """Run commands to remotly, pass a file and uncompress it"""
     if not os.path.isfile(archive_path):
@@ -49,17 +51,27 @@ def do_deploy(archive_path):
     file_ext = name[-1]
     fname, exten = os.path.splitext(file_ext)
 
+    tar1 = "sudo tar -xzf /tmp/{} -C ".format(file_ext)
+    tar2 = "/data/web_static/releases/{}/".format(fname)
+    f_tar = tar1 + tar2
+
+    mv1 = "sudo mv /data/web_static/releases/{}/web_static/*".format(fname)
+    mv2 = "/data/web_static/releases/{}/".format(fname)
+    f_mv = mv1 + mv2
+
+    ln = "sudo ln -s /data/web_static/releases/{}/ ".format(fname)
+    f_ln = ln + "/data/web_static/current"
+
     try:
         put(archive_path, "/tmp/")
         run("sudo mkdir -p /data/web_static/releases/{}/".format(fname))
-        run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(file_ext,
-                                                                       fname))
+        run(f_tar)
         run("sudo rm /tmp/{}".format(file_ext))
-        run("sudo mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(fname, fname))
-        run("sudo rm -rf /data/web_static/releases/{}/web_static".format(fname))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current".
+        run(f_mv)
+        run("sudo rm -rf /data/web_static/releases/{}/web_static".
             format(fname))
+        run("sudo rm -rf /data/web_static/current")
+        run(f_ln)
 
         print("New version deployed!")
         return True
